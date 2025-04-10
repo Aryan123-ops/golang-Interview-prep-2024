e5fc0185942e2da81719198d2d0f82e2957fcdce
Aryan Kumar <54620669+Aryan123-ops@users.noreply.github.com>
2025-04-10 18:20:48 +0530

    Update threesum.go

--- arrayManipulation/threesum.go
+++ arrayManipulation/threesum.go
@@ -65,3 +65,32 @@ func threesum(arr []int, target int) (int, int, int) {
 	}
 	return -1, -1, -1
 }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	arr := []int{3, 2, 4, 5, 8}
+// 	target := 13
+// 	i, j, k := threeSum(arr, target)
+// 	fmt.Println(i, j, k)
+// }
+
+// func threeSum(arr []int, target int) (int, int, int) {
+// 	n := len(arr)
+// 	for i := 0; i < n-2; i++ {
+// 		// Fix the first number, and look for two-sum for the remaining
+// 		indexmap := make(map[int]int)
+// 		currentTarget := target - arr[i]
+
+// 		for j := i + 1; j < n; j++ {
+// 			complement := currentTarget - arr[j]
+// 			if idx, ok := indexmap[complement]; ok {
+// 				return i, idx, j
+// 			}
+// 			indexmap[arr[j]] = j
+// 		}
+// 	}
+// 	return -1, -1, -1
+// }
