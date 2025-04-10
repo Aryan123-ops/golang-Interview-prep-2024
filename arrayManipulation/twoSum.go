1b20eae1b5726c385725e9640e1ee801d583b5fb
Aryan Kumar <54620669+Aryan123-ops@users.noreply.github.com>
2025-04-10 12:12:23 +0530

    Update twoSum.go

--- arrayManipulation/twoSum.go
+++ arrayManipulation/twoSum.go
@@ -75,3 +75,29 @@ func TwoSum(arr []int, target int) (int, int) {
 // 	// Step 5: If no complement is found, return nil.
 // 	return nil
 // }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{1, 2, 3, 4, 5}
+// 	target := 5
+// 	i, j := twosum(arr, target)
+// 	fmt.Println(i, j) // prints the indices
+// }
+
+// func twosum(arr []int, target int) (int, int) {
+// 	indexMap := make(map[int]int) // value -> index
+
+// 	for i, num := range arr {
+// 		complement := target - num
+// 		if idx, ok := indexMap[complement]; ok {
+// 			return idx, i
+// 		}
+// 		indexMap[num] = i
+// 	}
+// 	return -1, -1
+// }
