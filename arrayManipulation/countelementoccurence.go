599ffa9ec5986d9fc6ee599d5346eceaed82faa7
Aryan Kumar <aryan.kumar9911@gmail.com>
2025-06-03 13:25:06 +0530

    added to count occurence of all string

--- arrayManipulation/countelementoccurence.go
+++ arrayManipulation/countelementoccurence.go
@@ -1,17 +1,33 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	arr := []int{1, 1, 2, 2, 3, 4, 5, 5, 5}
+
+// 	countMap := make(map[int]int)
+
+// 	for _, num := range arr {
+// 		countMap[num]++
+// 	}
+
+// 	for k, num := range countMap {
+// 		fmt.Printf("%d:%d\n", k, num)
+// 	}
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	arr := []int{1, 1, 2, 2, 3, 4, 5, 5, 5}
-
-	countMap := make(map[int]int)
-
-	for _, num := range arr {
+	arr := "abbcdd"
+	runes := []rune(arr)
+	countMap := make(map[rune]int)
+	for _, num := range runes {
 		countMap[num]++
 	}
-
-	for k, num := range countMap {
-		fmt.Printf("%d:%d\n", k, num)
+	for k, v := range countMap {
+		fmt.Printf("%c:%d\n", k, v)
 	}
 }
