f79547cdd8c732a17595b6e1237b966e54a3bc39
Aryan Kumar <aryan.kumar9911@gmail.com>
2025-06-03 15:23:36 +0530

    added logic to print all elements from stack

--- dsa/stack.go
+++ dsa/stack.go
@@ -7,6 +7,13 @@ func main() {
 	stack.Push(10) // inserting element to stack at first time.
 	stack.Push(20) // inserting element to stack second time.
 	fmt.Println(stack.Pop())
+	for { // for loop to print all elements from stack
+		elem, ok := stack.Pop()
+		if !ok {
+			break
+		}
+		fmt.Println(elem)
+	}
 }
 
 type Stack[T any] struct {
