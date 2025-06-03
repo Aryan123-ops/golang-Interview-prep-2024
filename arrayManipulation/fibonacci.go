cb1e7608540eb339ac6bf2ff074cfcf2fcbf6719
Aryan Kumar <aryan.kumar9911@gmail.com>
2025-06-03 11:45:34 +0530

    some-modification

--- arrayManipulation/fibonacci.go
+++ arrayManipulation/fibonacci.go
@@ -13,21 +13,3 @@ func fibonacci(n int) int {
 		return fibonacci(n-1) + fibonacci(n-2)
 	}
 }
-
-// Online Go compiler to run Golang program online
-// Print "Try programiz.pro" message
-
-package main
-import "fmt"
-
-func main() {
-  fmt.Println("Try programiz.pro")
-  fmt.Println(factorial(7))
-}
-func factorial(n int) int {
-    if n == 0 {
-        return 1
-    }else {
-        return factorial(n-1) + factorial(n-2)
-    }
-}
